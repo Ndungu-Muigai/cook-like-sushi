68afbfc618e886e040db42e26d0ac8d52c62496f
Ndungu Muigai <ndungu.muigai01@gmail.com>
2026-04-06 11:10:16 +0300

    Replaced the icons with react icons

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,6 +4,13 @@ import { useState } from "react"
 import emailjs from "@emailjs/browser"
 import { toast } from "react-toastify"
 
+import { FaPhoneVolume } from "react-icons/fa6"
+import { HiOutlineMail } from "react-icons/hi"
+import { MdLocationOn } from "react-icons/md"
+import { FaInstagram } from "react-icons/fa"
+import { BsTwitterX } from "react-icons/bs";
+
+
 const Contact = ({ handleMouseMove, spot }) =>
 {
   //State to check if the message is being sent
@@ -51,20 +58,23 @@ const Contact = ({ handleMouseMove, spot }) =>
   const contactInfo = [
     {
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-        </svg>
+        <FaPhoneVolume className="h-5 w-5 sm:h-6 sm:w-6"/>
       ),
-      title: "Email",
-      value: "ndungu.muigai01@gmail.com",
-      link: "mailto:ndungu.muigai01@gmail.com"
+      title: "Call us",
+      value: "+254103895767",
+      link: "tel:+254103895767"
     },
     {
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-        </svg>
+        <HiOutlineMail className="h-5 w-5 sm:h-6 sm:w-6"/>
+      ),
+      title: "Email",
+      value: "cooklikesushi30@gmail.com",
+      link: "mailto:cooklikesushi30@gmail.com"
+    },
+    {
+      icon: (
+        <MdLocationOn className="h-5 w-5 sm:h-6 sm:w-6"/>
       ),
       title: "Location",
       value: "Nairobi, Kenya"
@@ -75,18 +85,14 @@ const Contact = ({ handleMouseMove, spot }) =>
     {
       name: "Twitter",
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-        </svg>
+        <BsTwitterX className="h-4 w-4 sm:h-5 sm:w-5"/>
       ),
-      link: "https://twitter.com/MzeeMuigai"
+      link: "https://twitter.com/"
     },
     {
       name: "Instagram",
       icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
-        </svg>
+        <FaInstagram className="h-4 w-4 sm:h-5 sm:w-5"/>
       ),
       link: "https://instagram.com/cooklikesushi_"
     }
